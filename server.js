59260a75277722c618e4c66b371e58cf3c5277d6
Ryan <thisianemailaddressthatis@gmail.com>
2022-08-29 15:25:13 -0500

    made require for unique id and write file for db.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
-const notesDB = require("./db/db.json");
+const notesDB = require("./db/db.json") || [];
+var uniqid = require("uniqid");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 
@@ -25,10 +27,17 @@ app.get("/api/notes", (req, res) => res.json(notesDB));
 
 app.post("/api/notes", (req, res) => {
   const { body } = req;
+  body.id = uniqid();
   notesDB.push(body);
   res.json(notesDB);
-  console.log(notesDB);
+  writeToFile("./db/db.json", notesDB);
 });
+
+const writeToFile = (destination, content) => {
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+};
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
